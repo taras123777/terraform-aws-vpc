e51f5a7a3fe607c0f557e5879c127d9e81370acc
Taras Siamro <96303219+taras123777@users.noreply.github.com>
2022-03-21 20:30:54 -0400

    ad

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.priavete_subnets_cidr[0]
+  cidr_block = var.private_subnets_cidr[0]
 }
 
 
@@ -8,11 +8,11 @@ resource "aws_subnet" "private1" {
 
 resource "aws_subnet" "private2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.priavete_subnets_cidr[1]
+  cidr_block = var.private_subnets_cidr[1]
 }
 
 
 resource "aws_subnet" "private3" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.priavete_subnets_cidr[2]
+  cidr_block = var.private_subnets_cidr[2]
 }
